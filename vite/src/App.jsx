8c6b2f48cbc292d32d7bbce3c39ef2fc288f3ed3
min8802 <ww330@naver.com>
2024-05-23 16:31:53 +0900

    send token

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,11 +1,17 @@
-import { Contract, ethers, formatEther, formatUnits } from "ethers";
+import { Contract, ethers, formatEther, parseEther } from "ethers";
 import { useEffect, useState } from "react";
 import abi from './abi.json';
 
 const App = () => {
   const [signer, setSigner] = useState();
   const [contract, setContract] = useState();
+  const [name, setName] = useState();
   const [totalSupply, setTotalSupply] = useState();
+  const [myBalance, setMyBalance] = useState();
+  const [symbol, setSymbol] = useState();
+  const [sendAddress, setSendAddress] = useState("");
+  const [sendToken, setSendToken] = useState("");
+
 
   const onClickMetamask = async () => {
     try {
@@ -23,20 +29,56 @@ const App = () => {
     setSigner(null);
     setContract(null);
     setTotalSupply(null);
+    setName(null);
+    setSymbol(null);
+    setMyBalance(null);
+  };
+
+  const onClickName = async () => {
+    try {
+      const response = await contract.name();
+      setName(response);
+     
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const onClickTotalSupply = async () => {
     try {
       const response = await contract.totalSupply();
-
-      console.log(response);
-
-      const parsedResponse = formatEther(response);
-
-      console.log(parsedResponse);
-
       setTotalSupply(response);
 
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  const onClickMyBalance = async () => {
+    try {
+      const response = await contract.balanceOf(signer.address);
+      const parsedResponse = formatEther(response);
+      setMyBalance(parsedResponse);
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onClickSendToken = async () => {
+    try {
+      if(!sendAddress || !sendToken) return;
+      const result = await contract.transfer(sendAddress, parseEther(sendToken, "wei"));
+      console.log(result)
+    } catch (error) {
+      console.error(error)
+    };
+  };
+
+  const getSymbol = async () => {
+    try {
+      const response = await contract.symbol();
+      setSymbol(response);
     } catch (error) {
       console.error(error);
     }
@@ -48,7 +90,12 @@ const App = () => {
     setContract(new Contract("0x93F6eA6D0F0Fd05963C131db47257BA61FBF436b", abi, signer));
   }, [signer]);
 
-  useEffect(() => {console.log(contract)},[contract])
+  useEffect(() => {
+    if (!contract) return;
+    getSymbol();
+  },[contract])
+
+  
 
   return (
     <div className="bg-red-100 min-h-screen flex justify-start items-center flex-col py-16">
@@ -72,13 +119,39 @@ const App = () => {
       )}
       {contract && <div className="mt-16 flex flex-col gap-8 bg-blue-100 grow max-w-md w-full">
         <h1 className="box-style">스마트 컨트랙트 연결을 완료했습니다.</h1>
-        <div className="flex">
-          <div className="box-style grow flex items-center">
-                {totalSupply
-                  ? `총 발행량: ${formatEther(totalSupply)}ETH`
-                  : "총 발행량 확인"}
+        <div className="flex flex-col gap-8">
+          <div className="flex w-full">
+              <div className="box-style grow flex items-center">
+                    {name
+                      ? `코인이름: ${name}`
+                      : "확인버튼을 눌러주세요."}
+                </div>
+              <button className="button-style ml-4" onClick={onClickName}>확인</button>
             </div>
-          <button className="button-style ml-4" onClick={onClickTotalSupply}>확인</button>
+          <div className="flex w-full">
+            <div className="box-style grow flex items-center">
+                  {totalSupply
+                    ? `총 발행량: ${formatEther(totalSupply)}${symbol}`
+                    : "총 발행량 확인"}
+              </div>
+            <button className="button-style ml-4" onClick={onClickTotalSupply}>확인</button>
+          </div>
+          <div className="flex w-full">
+            <div className="box-style grow flex items-center">
+                  {myBalance
+                    ? `잔액: ${myBalance}${symbol}`
+                    : "잔액 확인"}
+              </div>
+            <button className="button-style ml-4" onClick={onClickMyBalance}>확인</button>
+          </div>
+          <div className="flex w-full items-end">
+            <div className="flex flex-col gap-2 grow">
+              <div className="ml-1 text-lg font-bold">토큰 전송</div>
+              <input className="input-style" type="text" placeholder="지갑주소" value={sendAddress} onChange={(e) => {setSendAddress(e.target.value)}}/>
+              <input className="input-style" type="text" placeholder={`${symbol}을 입력하세요.`} value={sendToken} onChange={(e) => {setSendToken(e.target.value)}}/>
+            </div>
+              <button className="button-style ml-4" onClick={onClickSendToken}>확인</button>
+          </div>
         </div>
         </div>}
     </div>
